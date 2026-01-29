c2e65b220828958675ce52082a59d38a89d68afc
douglascgoncalves <douglascruzgoncalves@gmail.com>
2026-01-29 01:07:33 -0300

    Commit inicial
    Commit inicial do projeto - LancamentosBancariosAPI

---
+++ src/main/java/br/com/desafio/lancamentosbancariosapi/LancamentosBancariosApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.desafio.lancamentosbancariosapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancamentosBancariosApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancamentosBancariosApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/desafio/lancamentosbancariosapi/LancamentosBancariosApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.desafio.lancamentosbancariosapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancamentosBancariosApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
